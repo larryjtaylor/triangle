5c850ddbd17d30fbe2c90d8a4dd020fa5341d62f
Larry Taylor and Max Scher <larry@>
2017-06-01 16:34:37 -0700

    add math to produce result in .js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,20 @@
 $(document).ready(function() {
-  $(".triangle-sides").submit(function(event) {
+  $("#triangle-sides").submit(function(event) {
     event.preventDefault();
 
-    
+    var sideA = parseInt($("input#side-a").val());
+    var sideB = parseInt($("input#side-b").val());
+    var sideC = parseInt($("input#side-c").val());
+
+    if (sideA === sideB && sideA === sideC) {
+      $("#equilateral").show();
+    } else if (sideA === sideB || sideA === sideC || sideB === sideC) {
+      $("#isosceles").show();
+    } else if (sideA !== sideB && sideA !== sideC && sideB !== sideC) {
+      $("#scalene").show();
+    } else {
+      $("#not").show();
+    }
+
   });
 });
